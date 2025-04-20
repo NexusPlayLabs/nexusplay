b0dee3c992c7a78026a9520fe1c449374e99efa5
NexusPlayLabs <kronikzcrypto@centrum.cz>
2025-04-20 18:54:47 +0200

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -91,7 +91,7 @@ export default function Header() {
       <StyledHeader>
         <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
           <Logo to="/">
-            <img alt="Gamba logo" src="/logo.svg" />
+            <img alt="NexusPlay logo" src="/logo_kruhove.png" />
           </Logo>
         </div>
         <div style={{ display: 'flex', gap: '10px', alignItems: 'center', position: 'relative' }}>
