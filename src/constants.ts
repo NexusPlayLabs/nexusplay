a7d38b1470b9546095f4570742ee9b626f67f05a
NexusPlayLabs <kronikzcrypto@centrum.cz>
2025-04-20 18:41:19 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -22,7 +22,7 @@ export const PLATFORM_CREATOR_FEE = 0.01 // 1% !!max 5%!!
 export const PLATFORM_JACKPOT_FEE = 0.001 // 0.1%
 
 // Referral fee (in %)
-export const PLATFORM_REFERRAL_FEE = 0.05 // 0.25%
+export const PLATFORM_REFERRAL_FEE = 0.0025 // 0.25%
 
 /** If the user should be able to revoke an invite after they've accepted an invite */
 export const PLATFORM_ALLOW_REFERRER_REMOVAL = true
@@ -39,23 +39,44 @@ const lp = (tokenMint: PublicKey | string, poolAuthority?: PublicKey | string): 
  * For private pools, add the creator of the Liquidity Pool as a second argument
  */
 export const POOLS = [
+  // Fake token:
+  lp(FAKE_TOKEN_MINT),
+  // SOL:
   lp('So11111111111111111111111111111111111111112'),
-];
+  // USDC:
+  lp('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'),
+  // Wormhole:
+  lp('85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ'),
+  lp('H8cstTfTxPEm5qP3UXgga8Bdzm2MCDGAghJTgovPy6Y1', 'H83nsJJe11WY7TjhiVoDq5xmiYs7rU2iY4FweJuahVz2'),
+]
 
-export const DEFAULT_POOL = POOLS[0];
+// The default token to be selected
+export const DEFAULT_POOL = POOLS[0]
 
-export const TOKEN_METADATA: (Partial<TokenMeta> & { mint: PublicKey })[] = [
+/**
+ * List of token metadata for the supported tokens
+ * Alternatively, we can provide a fetcher method to automatically fetch metdata. See TOKEN_METADATA_FETCHER below.
+ */
+export const TOKEN_METADATA: (Partial<TokenMeta> & {mint: PublicKey})[] = [
   {
-    mint: new PublicKey('So11111111111111111111111111111111111111112'),
-    name: 'Solana',
-    symbol: 'SOL',
-    image: '/solana-sol-seeklogo.png',
+    mint: FAKE_TOKEN_MINT,
+    name: 'Fake',
+    symbol: 'FAKE',
+    image: '/fakemoney.png',
     baseWager: 1e9,
     decimals: 9,
     usdPrice: 0,
   },
-];
-
+  {
+    mint: new PublicKey('85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ'),
+    name: 'W',
+    symbol: 'Wormhole',
+    image: 'https://wormhole.com/token.png',
+    baseWager: 1e6,
+    decimals: 6,
+    usdPrice: 0,
+  },
+]
 
 /** HTML to display to user that they need to accept in order to continue */
 export const TOS_HTML = `
