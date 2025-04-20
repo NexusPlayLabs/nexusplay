d3db20b77f060a694757365ac0443c500a6415d8
NexusPlayLabs <kronikzcrypto@centrum.cz>
2025-04-20 18:02:36 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,52 +1,78 @@
 import { PublicKey } from '@solana/web3.js'
-import { PoolToken, TokenMeta } from 'gamba-react-ui-v2'
+import { FAKE_TOKEN_MINT, PoolToken, TokenMeta, makeHeliusTokenFetcher } from 'gamba-react-ui-v2'
 
-// RPC endpoint pre testnet
-export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? 'https://api.testnet.solana.com'
+// Get RPC from the .env file or default to the public RPC.
+export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? 'https://api.mainnet-beta.solana.com'
 
-// Adresa tvorcu platformy (môžete zmeniť na svoju testnet peňaženku)
+// Solana address that will receive fees when somebody plays on this platform
 export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
   'BgFTqkr4UPyAiyycQypBeWaCChKEhNCW67QbhJTPCf2e',
 )
 
-// URL pre Gamba explorer a zdieľanie (voliteľné)
+// Gamba explorer URL - Appears in RecentPlays
 export const EXPLORER_URL = 'https://explorer.gamba.so'
+
+// Platform URL - Appears in ShareModal
 export const PLATFORM_SHARABLE_URL = 'play.gamba.so'
 
-// Poplatky platformy
-export const PLATFORM_CREATOR_FEE = 0.01 // 1%
+// Creator fee (in %)
+export const PLATFORM_CREATOR_FEE = 0.01 // 1% !!max 5%!!
+
+// Jackpot fee (in %)
 export const PLATFORM_JACKPOT_FEE = 0.001 // 0.1%
+
+// Referral fee (in %)
 export const PLATFORM_REFERRAL_FEE = 0.0025 // 0.25%
+
+/** If the user should be able to revoke an invite after they've accepted an invite */
 export const PLATFORM_ALLOW_REFERRER_REMOVAL = true
 
-// Pomocná funkcia na definovanie poolu
+// Just a helper function
 const lp = (tokenMint: PublicKey | string, poolAuthority?: PublicKey | string): PoolToken => ({
   token: new PublicKey(tokenMint),
   authority: poolAuthority !== undefined ? new PublicKey(poolAuthority) : undefined,
 })
 
-// Zoznam podporovaných poolov – iba SOL
+/**
+ * List of pools supported by this platform
+ * Make sure the token you want to list has a corresponding pool on https://explorer.gamba.so/pools
+ * For private pools, add the creator of the Liquidity Pool as a second argument
+ */
 export const POOLS = [
-  lp('So11111111111111111111111111111111111111112'), // SOL mint adresa
+  // SOL:
+  lp('So11111111111111111111111111111111111111112'),
 ]
 
+// The default token to be selected
 export const DEFAULT_POOL = POOLS[0]
 
-// Metadata tokenov – iba SOL
-export const TOKEN_METADATA: (Partial<TokenMeta> & { mint: PublicKey })[] = [
+/**
+ * List of token metadata for the supported tokens
+ * Alternatively, we can provide a fetcher method to automatically fetch metdata. See TOKEN_METADATA_FETCHER below.
+ */
+export const TOKEN_METADATA: (Partial<TokenMeta> & {mint: PublicKey})[] = [
   {
-    mint: new PublicKey('So11111111111111111111111111111111111111112'),
-    name: 'Solana',
-    symbol: 'SOL',
-    image: '/solana.png',
-    baseWager: 1e9, // 1 SOL
+    mint: FAKE_TOKEN_MINT,
+    name: 'Fake',
+    symbol: 'FAKE',
+    image: '/fakemoney.png',
+    baseWager: 1e9,
     decimals: 9,
-    usdPrice: 0, // Voliteľné: cena v USD
+    usdPrice: 0,
+  },
+  {
+    mint: new PublicKey('85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ'),
+    name: 'W',
+    symbol: 'Wormhole',
+    image: 'https://wormhole.com/token.png',
+    baseWager: 1e6,
+    decimals: 6,
+    usdPrice: 0,
   },
 ]
 
-// HTML pre podmienky používania
-export const TOS_HTML = `
+/** HTML to display to user that they need to accept in order to continue */
+export const TOS_HTML = 
   <p><b>1. Age Requirement:</b> Must be at least 18 years old.</p>
   <p><b>2. Legal Compliance:</b> Follow local laws responsibly.</p>
   <p><b>3. Risk Acknowledgement:</b> Games involve risk; no guaranteed winnings.</p>
@@ -56,4 +82,19 @@ export const TOS_HTML = `
   <p><b>7. Fair Play:</b> Games are conducted fairly and transparently.</p>
   <p><b>8. Data Privacy:</b> Your privacy is important to us.</p>
   <p><b>9. Responsible Gaming:</b> Play responsibly; seek help if needed.</p>
-`
+
+
+/**
+ * A method for automatically fetching Token Metadata.
+ * Here we create a fetcher that uses Helius metadata API, if an API key exists as an environment variable.
+ */
+export const TOKEN_METADATA_FETCHER = (
+  () => {
+    if (import.meta.env.VITE_HELIUS_API_KEY) {
+      return makeHeliusTokenFetcher(
+        import.meta.env.VITE_HELIUS_API_KEY,
+        { dollarBaseWager: 1 },
+      )
+    }
+  }
+)()
