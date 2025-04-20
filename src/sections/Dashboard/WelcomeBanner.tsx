4fcb29fd94eb6e9d388f772e1d5f08cfa5123368
NexusPlayLabs <kronikzcrypto@centrum.cz>
2025-04-20 19:37:20 +0200

    Update WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -40,31 +40,9 @@ const Buttons = styled.div`
 `
 
 const Welcome = styled.div`
-  @keyframes welcome-fade-in {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
-
-  @keyframes backgroundGradient {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-
-  /* Updated background image */
-  background: url('/welcome_background.png') no-repeat center center fixed;
+  background: url('/welcome_background.png') no-repeat center center;
   background-size: cover;
-  animation: welcome-fade-in .5s ease;
+  animation: welcome-fade-in 0.5s ease, backgroundGradient 30s ease infinite;
   border-radius: 10px;
   position: relative;
   overflow: hidden;
@@ -73,7 +51,7 @@ const Welcome = styled.div`
   justify-content: center;
   flex-direction: column;
   padding: 20px;
-  filter: drop-shadow(0 4px 3px rgba(0,0,0,.07)) drop-shadow(0 2px 2px rgba(0,0,0,.06));
+  filter: drop-shadow(0 4px 3px rgba(0, 0, 0, 0.07)) drop-shadow(0 2px 2px rgba(0, 0, 0, 0.06));
 
   & img {
     animation-duration: 5s;
@@ -83,13 +61,17 @@ const Welcome = styled.div`
     height: 100px;
     top: 0;
     right: 0;
-    &:nth-child(1) {animation-delay: 0s;}
-    &:nth-child(2) {animation-delay: 1s;}
+    &:nth-child(1) {
+      animation-delay: 0s;
+    }
+    &:nth-child(2) {
+      animation-delay: 1s;
+    }
   }
 
   & > div {
     padding: 0px;
-    filter: drop-shadow(0 4px 3px rgba(0,0,0,.07)) drop-shadow(0 2px 2px rgba(0,0,0,.06));
+    filter: drop-shadow(0 4px 3px rgba(0, 0, 0, 0.07)) drop-shadow(0 2px 2px rgba(0, 0, 0, 0.06));
   }
 
   @media (min-width: 800px) {
@@ -101,6 +83,7 @@ const Welcome = styled.div`
     }
   }
 `
+
 
 export function WelcomeBanner() {
   const wallet = useWallet()
