b4973d11bf104225284b7326de8953cad43d56f1
NexusPlayLabs <kronikzcrypto@centrum.cz>
2025-04-24 17:58:17 +0200

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -13,7 +13,7 @@ import TokenSelect from './TokenSelect'
 import ConnectModal from '../components/ConnectModal'
 import { useWalletModal } from '@solana/wallet-adapter-react-ui'
 import { useWallet }      from '@solana/wallet-adapter-react'
-import Modal              from '../components/Modal'
+import { Modal }         from '../components/Modal'
 
 const Bonus = styled.button`
   all: unset;
