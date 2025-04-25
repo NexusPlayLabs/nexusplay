968458d93444e909667f854d0a3ae2bdc6627841
NexusPlayLabs <kronikzcrypto@centrum.cz>
2025-04-25 17:37:04 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 const ENV_PREFIX = ['VITE_']
 
-export default defineConfig(() => ({
+export default defineConfig({
   envPrefix: ENV_PREFIX,
   server: { port: 4001, host: false },
   assetsInclude: ["**/*.glb"],
@@ -16,17 +16,17 @@ export default defineConfig(() => ({
     },
   },
 
-  // <-- TU DOPLŇ tieto sekcie:
   optimizeDeps: {
     include: ['bs58'],
   },
   build: {
-    rollupOptions: {
-      external: ['bs58'],
+    // commonjsOptions zabezpečí správne bundlovanie CJS modulov ako bs58
+    commonjsOptions: {
+      include: [/bs58/, /node_modules/],
     },
   },
 
   plugins: [
     react({ jsxRuntime: 'classic' }),
   ],
-}))
+})
