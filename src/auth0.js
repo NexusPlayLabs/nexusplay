eb2ae9bf1449cdf8017b9a6b83de0d6ef037f056
NexusPlayLabs <kronikzcrypto@centrum.cz>
2025-04-25 18:33:51 +0200

    Create auth0.js

---
+++ src/auth0.js
@@ -0,0 +1,37 @@
+// src/auth0.js
+import { createContext, useContext, useState, useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Auth0Context = createContext();
+
+export const Auth0Provider = ({ children }) => {
+  const { loginWithRedirect, logout, user, isAuthenticated, isLoading, error } = useAuth0();
+
+  const [auth0User, setAuth0User] = useState(null);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      setAuth0User(user);
+    }
+  }, [isAuthenticated, user]);
+
+  const login = () => loginWithRedirect();
+  const signOut = () => logout({ returnTo: window.location.origin });
+
+  return (
+    <Auth0Context.Provider
+      value={{
+        isAuthenticated,
+        isLoading,
+        user: auth0User,
+        login,
+        signOut,
+        error,
+      }}
+    >
+      {children}
+    </Auth0Context.Provider>
+  );
+};
+
+export const useAuth0Context = () => useContext(Auth0Context);
